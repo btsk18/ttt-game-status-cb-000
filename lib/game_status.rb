709aceeda6a18b0032cc5f4f927c7e4810dd5506
Brandon Leeds <brandonleeds@gmail.com>
2017-11-07 00:16:44 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,25 @@
-# Helper Method
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def won?(board)
+  # winningCombo = nil
+  WIN_COMBINATIONS.each { |combo|
+    o_combo = combo.select { |i| board[i] == "O" }
+    x_combo = combo.select { |i| board[i] == "X" }
+    return o_combo if o_combo.count == 3
+    return x_combo if x_combo.count == 3
+  }
+  return false
 end
 
-# Define your WIN_COMBINATIONS constant
+def full?(board)
+  return board.reject { |e| e == " "  }
+end
